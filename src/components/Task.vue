5f1c5d8ea36ab5d6b7f462c01b546204703450c8
Baptajck <parville.baptiste@gmail.com>
2020-04-28 18:22:59 +0200

    Début de la TodoList

---
+++ src/components/Task.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="task">
+    <ul>
+      <li v-for:="tasks in task" class="task-item">{{ task }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tasks: String
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,39 @@
+/* eslint-disable vue/valid-v-for */
 <template>
   <div>
     <h1 class="home-title">VueJs</h1>
     <p class="home-desc">Je suis la page <strong class="home-desc--strong">{{ titlePage() }}</strong></p>
     <div class="home-form">
-      <InputField
-        type="text"
-        label="Prénom"
-        placeholder="Votre prénom..."
-      />
-      <InputField
-        type="text"
-        label="Nom"
-        placeholder="Votre nom..."
-      />
+    <div class="input">
+      <label class="input-label">Votre tâche</label>
+      <input placeholder="Votre tâche..." type="text" v-model="value" class="input-input"/>
     </div>
+      <button v-on:click="pushTask(value)">Clique</button>
+    </div>
+    <div v-if="tasks.length > 0" class="home-form">
+      <ul>
+        <li v-for="(item, key) in tasks" :key="key" class="task-item">{{ item }}</li>
+      </ul>
+      </div>
     <a href="/register" class="home-link">Passer à la prochaine page</a>
   </div>
 </template>
 
 <script>
-import InputField from '@/components/InputField'
 export default {
-  name: 'Home',
-  components: {
-    InputField
+  data () {
+    return {
+      value: '',
+      id: 2,
+      tasks: []
+    }
   },
   methods: {
     titlePage () {
       return document.location.pathname
+    },
+    pushTask (value) {
+      this.tasks.push(value)
     }
   }
 }
